69dd756eaefa902add8e1c374716c169139b6fb3
noribang <hoonk9387@gmail.com>
2017-05-29 17:33:38 -0500

    add span element to .html and add .clickable class to .html, .css and .js jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 When <p> is clicked <img> of walrus will show.
 */
 $(document).ready(function() {
-	$('p').click(function() {
+	$('.clickable').click(function() {
 		$('.walrus-showing').toggle();
 		$('.walrus-hidden').toggle();
 	});
